5dc3e9cc49ca836cdc1d7eb562440e741d9c60e6
DESKTOP-LSSF610\PJH <jhpark@tssni.com>
2024-03-05 16:38:13 +0900

    First Commit

---
+++ Board.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ru_Algorithm
+{
+    class MyList<T>
+    {
+        const int DEFAULT_SIZE = 1;
+        T[] _data = new T[DEFAULT_SIZE];
+
+        public int Count = 0; //실제로 사용중인 데이터 개수
+        public int Capacity {  get { return _data.Length; } } // 예약된 데이터 개수
+
+        // !O(N) O(1) : 예외 케이스 : 이사 비용은 무시한다.
+        public void Add(T item)
+        {
+            //1. 공간이 충분히 남아 있는지 확인한다.
+            if (Count >= Capacity)
+            {
+                // 공간을 다시 늘려서 확보한다
+                T[] newArray = new T[Count * 2];
+                for (int i = 0; i < Count; i++)
+                    newArray[i] = _data[i];
+                _data = newArray;
+            }
+
+            // 2. 공간에다가 데이터를 넣어준다.
+            _data[Count] = item;
+            Count++;
+        }
+
+        // O(1)
+        // +a 인덱스
+        public T this[int index]
+        {
+            get { return _data[index];}
+            set { _data[index] = value;}
+        }
+
+        // O(N) : 최악의 경우까지 생각해서 (index가 0일때, N번 다 돌도록)
+        public void RemoveAt(int index)
+        {
+            // 101 102 104 105 0
+            for (int i = index; i < Count - 1; i++)
+                _data[i] = _data[i + 1];
+            _data[Count - 1] = default(T);
+            Count--;
+        }
+    }
+
+    internal class Board
+    {
+        public int[] _data = new int[25]; //배열
+        public MyList<int> _data2 = new MyList<int>(); //동적 배열
+        public LinkedList<int> _data3 = new LinkedList<int>(); //(이중)연결 리스트
+
+        public void Initialize()
+        {
+            _data2.Add(101);
+            _data2.Add(102);
+            _data2.Add(103);
+            _data2.Add(104);
+            _data2.Add(105);
+
+            int temp = _data2[2];
+
+            _data2.RemoveAt(2);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Ru_Algorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //함수 시작하자마자 initialize실행
+            Board board = new Board();
+            board.Initialize();
+
+            Console.CursorVisible = false;
+
+            const int WAIT_TICK = 1000 / 30;
+            const char CIRCLE = '\u25cf';
+
+            int lastTick = 0;
+
+            while (true)
+            {
+                #region 프레임 관리
+                //FPS 프레임 (60프레임 OK 30프레임 이하 NO)
+
+                /*              int currentTick = System.Environment.TickCount;
+                                int elapsedTick = currentTick - lastTick;
+                                // 만약에 경과한 시간이 1/30초보다 작다면(TickCount는 milesecend단위라서 1초가 1000ms)
+                                if (elapsedTick < 1000 / 30)
+                                {
+                                    continue;
+                                }
+                                lastTick = currentTick;*/
+
+                // 만약에 경과한 시간이 1/30초보다 작다면 -> 용이하게 수정
+                int currentTick = Environment.TickCount;
+                if (currentTick - lastTick < WAIT_TICK)
+                {
+                    continue;
+                }
+                lastTick = currentTick;
+                #endregion
+
+                //입력
+                //로직
+                //렌더링
+
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine( "Hello World!" );
+
+                for ( int i = 0; i < 25; i++)
+                {
+                    for ( int j = 0; j < 25; j++ )
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.Write(CIRCLE);
+                    }
+                    Console.WriteLine(); //25개마다 줄넘기
+                }
+            }
+        }
+    }
+}
